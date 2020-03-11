7d7d335350de65d94a73f0a1b4a45539d7fc92d2
minhnguyenbg <=minhdt3.k9@gmail.com>
2020-03-12 00:08:38 +0700

    Add ModelBase

---
+++ GameTv/GameTv/GameTv/AppContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameTv
+{
+   public class AppContext
+    {
+    }
+}
---
+++ GameTv/GameTv/GameTv/ViewModels/Base/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using GameTv.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameTv.ViewModels.Base
+{
+    public class ViewModelBase:BindableBase
+    {
+    }
+}
